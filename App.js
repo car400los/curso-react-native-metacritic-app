6f4e1ca0243273e091768f40c39f865c3d051c44
D4nk <asta.fcb@yahoo.com>
2024-07-25 22:21:34 +0200

    Añadido el componente Image y añadida un imagen

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+import icon from './assets/icon.png';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Image source={icon} style={{ width: 100, height: 100 }} />
       <StatusBar style="auto" />
       <Text>Tenemos aquí la app</Text>
     </View>
