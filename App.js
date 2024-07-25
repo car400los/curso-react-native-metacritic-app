0924c06172b8b17a58455bd7c51fab6bf0917c03
D4nk <asta.fcb@yahoo.com>
2024-07-25 23:49:45 +0200

    Añadido de eslint, pretier, libreria de recursos de metacritic y cambios varios en la feed pricipal

--- App.js
+++ App.js
@@ -1,12 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { userState } from "react";
+import { StyleSheet, Text, View, Pressable} from 'react-native';
+import { getLatestGames } from './lib/metacritic';
 
 export default function App() {
+  const [games, setGames] = useState([]);
+
+  useEffect(() => {
+     getLatestGames().then((games) => 
+      setGames(games));
+  }
+
+
   return (
     <View style={styles.container}>
-      <Image source={{uri: "https://www.metacritic.com/a/img/catalog/provider/6/3/6-1-4763-13.jpg"}} style={{width: 215, height: 294}}/>
       <StatusBar style="light" />
-      <Text style={{color: 'white'}}>Tenemos aquí la app</Text>
+      <Image source={{uri: "https://www.metacritic.com/a/img/catalog/provider/6/3/6-1-4763-13.jpg"}} style={{width: 215, height: 294}}/>
+
+       <TouchableHighlight
+        underlayColor={"#09f"}
+        onPress={() => alert('Hola')}
+       >
+        <Text style={{color: 'white',}}>Pulsa aquí</Text>
+      </TouchableHighlight>
     </View>
   );
 }
