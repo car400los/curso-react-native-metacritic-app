db09183b3c08af24387e5eed82de602f1efea3d5
D4nk <asta.fcb@yahoo.com>
2024-07-25 22:11:31 +0200

    update de la feed principal

--- App.js
+++ App.js
@@ -4,8 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Text>Tenemos aquí la app</Text>
     </View>
   );
 }
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#09f',
     alignItems: 'center',
     justifyContent: 'center',
   },
